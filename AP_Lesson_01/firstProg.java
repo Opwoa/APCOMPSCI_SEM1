963b06a47b4d0c7ebfceda7791c501ef05fd3c1f
Opwoa <joshuahornilla@gmail.com>
2016-09-06 10:54:55 -0700

    Added ASCII Art Files

---
+++ AP_Lesson_01/firstProg.java
@@ -0,0 +1,14 @@
+public class firstProg {
+	public static void main(String[]args) {
+		System.out.println("                    .==.");
+		System.out.println("                   ()''()-.");
+		System.out.println("        .---.       ;--; /");
+		System.out.println("      .'_:___\". _..'.  __'.");
+		System.out.println("      |__ --==|'-''' \'...;");
+		System.out.println("      [  ]  :[|       |---\\");
+		System.out.println("      |__| I=[|     .'    '.");
+		System.out.println("      / / ____|     :       '._");
+		System.out.println("     |-/.____.'      | :       :");
+		System.out.println("    /___\\ /___\\      '-'._----'");
+	}
+}
---
+++ AP_Lesson_01/secondProg.java
@@ -0,0 +1,26 @@
+public class secondProg {
+	public static void main(String[]args) {
+		System.out.println("                       .-.");
+		System.out.println("                      |_:_|");
+		System.out.println("                     /(_Y_)\\");
+		System.out.println(".                   ( \\/M\\/ )");
+		System.out.println(" '.               _.'-/'-'\\-'._");
+		System.out.println("   ':           _/.--'[[[[]'--.\\_");
+		System.out.println("     ':        /_'  : |::\"| :  '.\\");
+		System.out.println("       ':     //   ./ |oUU| \\.'  :\\");
+		System.out.println("         ':  _:'..' \\_|___|_/ :   :|");
+		System.out.println("           ':.  .'  |_[___]_|  :.':\\");
+		System.out.println("            [::\\ |  :  | |  :   ; : \\");
+		System.out.println("             '-'   \\/'.| |.' \\  .;.' |");
+		System.out.println("             |\\_    \\  '-'   :       |");
+		System.out.println("             |  \\    \\ .:    :   |   |");
+		System.out.println("             |   \\    | '.   :    \\  |");
+		System.out.println("             /       \\   :. .;       |");
+		System.out.println("            /     |   |  :__/     :  \\\\");
+		System.out.println("           |  |   |    \\:   | \\   |   ||");
+		System.out.println("          /    \\  : :  |:   /  |__|   /|");
+		System.out.println("          |     : : :_/_|  /'._\\  '--|_\\");
+		System.out.println("          /___.-/_|-'   \\  \\");
+		System.out.println("                         '-'");
+	}
+}
