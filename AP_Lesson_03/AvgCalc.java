5dfca9eeae8d161a4a3deb108c670107b6ae101e
Opwoa <joshuahornilla@gmail.com>
2016-09-12 11:24:19 -0700

    Added last part of Lesson 3

---
+++ AP_Lesson_03/AvgCalc.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AvgCalc {
+	public static void main(String[]args) {
+		System.out.println("Hello! I can calculate th average of any data set you give me.");
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("How many pieces of data do you have?");
+		double n = keyboard.nextDouble();
+		double nMinusOne = n - 1;
+		
+		System.out.println("Enter each piece of data separated by a space");
+		double sum = 0;
+		for (int i = 0; i < n; i++) {
+			double currentNum = keyboard.nextDouble();
+			sum = sum + currentNum;
+			System.out.println(currentNum);
+		}
+		
+		double avg = sum / n;
+		System.out.println("Your Average is " + avg + ".");
+	}
+}
