5b0b14cf56c074d4a0b5a3882b7e5546e2d02d24
Opwoa <HornillaJ9651@sduhsd.lan>
2016-10-20 11:49:32 -0700

    finished my incredibly wonderful adventure game story

--- AP_Lesson_05/APLab_5.2/AdventureGame.java
+++ AP_Lesson_05/APLab_5.2/AdventureGame.java
@@ -22,31 +22,34 @@ public class AdventureGame {
 				System.out.println("First off, I hate you.");
 				System.out.println("Second off, all I've done so far is insult you so let's have a real story.");
 				System.out.println("Once upon a time there were a brother and sister named Jack and Jill.\nThey go up the hill to fetch a pail of water.");
-				System.out.println("Choose ending (\"1\") or ending (\"2\")");
+				System.out.println("Choose ending (\"1\") or ending (\"2\").");
 				if (keyboard.nextInt() == 1) {
 					System.out.println("Jack fell down and had permanent brain damage and Jill dropped dead at\nthe top from the poisoned well water and came tumbling down the hill.");
-					System.out.println("Ha! You filthy optimist.");
+					System.out.println("There. It's over. Thanks for playing.");
 				} else {
 					System.out.println("The writer got tired of coming up with stuff.\nSo, yeah. The End.");
 				}
 			}
 		} else {
 			System.out.println("Good. Life is dispair.");
-			System.out.println("");
+			System.out.println("Want to hear about a student (\"1\") or a teacher (\"2\")?");
 			if (keyboard.nextInt() == 1) {
-				System.out.println("");
-				System.out.println("");
+				System.out.println("A good student(\"1\") or a bad student(\"2\")?");
 				if (keyboard.nextInt() == 1) {
-					System.out.println("");
+					System.out.println("Well, I mean it's the same story just different perspectives.");
+					System.out.println("But yeah, the good student struggled in school and had to watch the\nbad student do well in everything by cheating.");
+					System.out.println("(It's actually a horror story.)");
 				} else {
-					System.out.println("");
+					System.out.println("The bad student goes through school and cheats but then life slaps\nhim in the face and he becomes homeless.");
+					System.out.println("The End.");
 				}
 			} else {
-				System.out.println("");
+				System.out.println("This teacher decided to use their sick days for a vacation.");
+				System.out.println("Choose ending (\"1\") or ending (\"2\").");
 				if (keyboard.nextInt() == 1) {
-					System.out.println("");
+					System.out.println("They went to the beach and got bitten by a shark.\nHighly unfortunate.");
 				} else {
-					System.out.println("");
+					System.out.println("They went skiing and actually got sick.\nThis set off a chain of health issues.\nIrony");
 				}
 			}
 		}
