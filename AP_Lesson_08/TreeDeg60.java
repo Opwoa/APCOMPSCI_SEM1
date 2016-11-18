8ccfbaf8f582bacec36ec5c535ed6ae1aaeacfad
Opwoa <HornillaJ9651@sduhsd.lan>
2016-11-18 10:46:31 -0800

    Finished all my english homework. oh and lesson 8

--- AP_Lesson_08/TreeDeg60.java
+++ AP_Lesson_08/TreeDeg60.java
@@ -4,7 +4,18 @@ public class TreeDeg60 {
 	public static void main(String[]args) {
 		Scanner keyboard = new Scanner(System.in);
 		
+		System.out.println("Enter a word");
+		String word = keyboard.next();
+		
+		tree(word, 1, word.length());
 	}
 	
-	public static 
+	public static String tree(String word, int start, int stop){
+		if (start <= stop) {
+			System.out.printf("%10s\n", word.substring(0, start));
+			return tree (word, start +1, stop);
+		} else {
+			return " ";
+		}
+	}
 }
